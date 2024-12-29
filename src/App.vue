1f9379c8a24250f4cf51edd407d335a70f8c1444
mksdesuyo <rizqimks@gmail.com>
2024-12-29 11:17:09 +0700

    get rid of the map thing cause it was really frustating. add some animation to the icons

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,10 @@ import { RouterView } from 'vue-router';
     <div class="flex items-center justify-between">
       <p class="text-sm">
         Made with
-        <font-awesome-icon :icon="['fas', 'heart']" class="duration-150 ease-in-out opacity-85" />
+        <font-awesome-icon
+          :icon="['fas', 'heart']"
+          class="duration-150 ease-in-out opacity-75 hover:opacity-100 hover:text-red-500"
+        />
         by MKS
       </p>
       <ul class="flex gap-3 text-sm">
@@ -42,31 +45,35 @@ import { RouterView } from 'vue-router';
           <a href="https://www.facebook.com/rizqi.mks.22" target="_blank" rel="noreferrer noopener">
             <font-awesome-icon
               :icon="['fab', 'facebook']"
-              class="duration-100 ease-in-out opacity-75 hover:opacity-85"
+              class="duration-150 ease-in-out opacity-75 hover:opacity-100 hover:text-[#1778F2]"
             />
           </a>
         </li>
         <li>
-          <a href="https://www.facebook.com/rizqi.mks.22" target="_blank" rel="noreferrer noopener">
+          <a href="https://github.com/mksdesuyo" target="_blank" rel="noreferrer noopener">
             <font-awesome-icon
               :icon="['fab', 'github']"
-              class="duration-100 ease-in-out opacity-75 hover:opacity-85"
+              class="duration-150 ease-in-out opacity-75 hover:opacity-100 hover:text-[#211f1f]"
             />
           </a>
         </li>
         <li>
-          <a href="https://www.facebook.com/rizqi.mks.22" target="_blank" rel="noreferrer noopener">
+          <a
+            href="https://www.instagram.com/houtarou_rizuki/"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
             <font-awesome-icon
               :icon="['fab', 'instagram']"
-              class="duration-100 ease-in-out opacity-75 hover:opacity-85"
+              class="duration-150 ease-in-out opacity-75 hover:opacity-100 hover:text-[#f44747]"
             />
           </a>
         </li>
         <li>
-          <a href="https://www.facebook.com/rizqi.mks.22" target="_blank" rel="noreferrer noopener">
+          <a href="https://www.linkedin.com/in/rizqimks" target="_blank" rel="noreferrer noopener">
             <font-awesome-icon
               :icon="['fab', 'linkedin']"
-              class="duration-100 ease-in-out opacity-75 hover:opacity-85"
+              class="duration-150 ease-in-out opacity-75 hover:opacity-100 hover:text-[#006192]"
             />
           </a>
         </li>
