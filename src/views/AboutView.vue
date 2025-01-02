92b61b758651e95d29a822eb0eec63888a775228
mksdesuyo <rizqimks@gmail.com>
2025-01-02 23:27:08 +0700

    feat(AboutView): adding content to it

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,38 @@
 <template>
-  <main class="grid gap-2 px-8 py-4 leading-5 place-content-center h-dvh">
-    <h1>about</h1>
+  <main class="grid gap-2 px-8 py-16 pt-8 leading-5 h-dvh">
+    <h1 class="pt-12 text-4xl">Who I am</h1>
+    <div class="w-3/5 mb-2 border-2 border-black rounded"></div>
+    <div class="pr-1 overflow-y-auto">
+      <p>
+        Greetings! I'm Rizqi Muhammad Khatami Sandi, a
+        <span class="font-bold">Junior Web Developer</span> certified by BNSP and based in Garut,
+        Indonesia.<br />
+        With a proper foundation in web development, I specialize in creating
+        <span class="font-bold">Front-End</span> solutions using
+        <span class="bg-green-100 px-1 text-[#41B883] rounded">VueJs</span> and developing
+        <span class="font-bold">Back-End</span> systems with
+        <span class="px-1 text-[#F05340] bg-red-100 rounded">Laravel</span>. <br />
+        <br />
+        Beyond coding, I have deep passion for learning new languages and cultures. I can
+        communicate in <span class="font-bold">English</span> and
+        <span class="font-bold">Japanese</span> at professional and conversational levels, along
+        with some understanding of <span class="font-bold">Arabic</span>. <br />
+        My enthusiasm for personal growth extends to physical activities as well—I enjoy
+        <span class="font-bold">weightlifting and calisthenics</span>,
+        <span class="font-bold">cycling</span>, and <span class="font-bold">swimming</span>, which
+        help me stay positive.
+        <br />
+        <br />
+        In addition, I've earned notable certifications including
+        <span class="font-bold">Google IT Support (Coursera)</span> and
+        <span class="font-bold"
+          >AWS Academy Graduate in AWS Cloud Foundations and Cloud Architecting</span
+        >, showcasing my versatility to continous learning.
+        <br />
+        <br />
+        I'm eager to collaborate and bring innovative ideas to life in the ever-evolving tech
+        industry.
+      </p>
+    </div>
   </main>
 </template>
